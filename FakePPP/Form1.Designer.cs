12263e581fadb153a9b6d94eb5f6b0a03d159d13
hst2016 <sally92@gmx.de>
2023-05-10 12:16:39 +0200

    Added Files

---
+++ FakePPP/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace FakePPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbName = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // tbName
+            // 
+            tbName.Location = new Point(537, 290);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(100, 23);
+            tbName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(471, 298);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(tbName);
+            Name = "Form1";
+            Text = "FakePPP";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbName;
+        private Label label1;
+    }
+}
---
+++ FakePPP/Form1.cs
@@ -0,0 +1,11 @@
+namespace FakePPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbName.Text = "Name Holle";
+        }
+    }
+}
